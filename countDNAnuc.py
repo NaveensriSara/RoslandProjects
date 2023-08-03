7581382899963c35ed3b1101a5b44dc0535897cb
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2023-08-03 00:16:17 -0400

    Update countDNAnuc.py

--- countDNAnuc.py
+++ countDNAnuc.py
@@ -11,15 +11,23 @@ T_count = 0
 for char in txt:
 	if char == 'A':
 		A_count += 1
+		#print(A_count)
 	elif char == 'T':
 		T_count += 1
+		#print(T_count)
 	elif char == 'C':
 		C_count += 1
-	else char == 'G':
+		#print(C_count)
+	else:
 		G_count += 1
-return 
+		#print(G_count)
 
 
 
+
+print("A Count:", A_count )
+print("G Count:", G_count )
+print("C Count:", C_count )
+print("T Count:", T_count )
 
 
