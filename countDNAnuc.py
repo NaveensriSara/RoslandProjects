860f69dc82a45ad0b32cfa772a961cea665e020f
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2023-08-02 23:52:42 -0400

    Create countDNAnuc.py

---
+++ countDNAnuc.py
@@ -0,0 +1,25 @@
+
+#given a length of string, find the number of occurances each letter 
+
+txt = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+
+A_count = 0
+G_count = 0
+C_count = 0
+T_count = 0
+
+for char in txt:
+	if char == 'A':
+		A_count += 1
+	elif char == 'T':
+		T_count += 1
+	elif char == 'C':
+		C_count += 1
+	else char == 'G':
+		G_count += 1
+return 
+
+
+
+
+
