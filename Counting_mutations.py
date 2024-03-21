18fdd89a135ddca66cb33fd561d4392e5109bcb5
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2024-03-20 22:28:20 -0400

    update

--- Counting_mutations.py
+++ Counting_mutations.py
@@ -1,26 +1,10 @@
-#Counting point mutations 
-
-str1= 'GAGCCTACTAACGGGAT'
-str2= 'CATCGTAATGACGGCCT'
-
-def Compare(str1,str2):
-   
-   count = 0
-   for i in range(len(str1)):  # Loop through the indexes of the strings
-        if str1[i] != str2[i]:  # Compare characters at index i in both strings
-            count += 1  # Increment count if characters are different
-    
-    return count 
-
-
 def Compare(str1, str2):
-    if len(str1) != len(str2):
-        print("Strings are of different lengths")
-        return
-    
-    count = 0  # Initialize count to 0
-    for i in range(len(str1)):  # Loop through the indexes of the strings
-        if str1[i] != str2[i]:  # Compare characters at index i in both strings
-            count += 1  # Increment count if characters are different
-    
+    count = 0
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            count += 1
     return count
+
+string1 = 'GAGCCTACTAACGGGAT'
+string2 = 'CATCGTAATGACGGCCT'
+print(Compare(string1, string2))
