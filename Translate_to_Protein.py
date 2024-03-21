4ad79dee2b0c93f9112786dbdf14b5f4c1704a1d
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2024-03-20 22:44:01 -0400

    update

--- Translate_to_Protein.py
+++ Translate_to_Protein.py
@@ -2,26 +2,30 @@
 def translate(sequence): 
        
     table = { 
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
-        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
-        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+        'AUA':'I', 'AUC':'I', 'AUU':'I', 'ATG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T', 
+        'AAC':'N', 'AAU':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGU':'S', 'AGA':'R', 'AGG':'R',                  
+        'CUA':'L', 'CTC':'L', 'CUG':'L', 'CUU':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P', 
+        'CAC':'H', 'CAU':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R', 
+        'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A', 
+        'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G', 
+        'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S', 
+        'UUC':'F', 'UUU':'F', 'UUA':'L', 'UUG':'L', 
+        'UAC':'Y', 'UAU':'Y', 'UAA':'_', 'UAG':'_', 
+        'UGC':'C', 'UGU':'C', 'UGA':'_', 'UGG':'W', 
+        'AUG':'M',
     } 
+    protein = ''
     for i in range(0, len(sequence), 3): 
             codon = sequence[i:i + 3] 
             protein+= table[codon] 
     return protein 
 
-print(translate(AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA))
+string = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+print(translate(string))
+
