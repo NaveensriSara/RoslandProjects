958583428e12448b2ff1aa25d9faac7e45892572
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2024-06-30 15:27:09 -0400

    updated project

--- Motif.py
+++ Motif.py
@@ -1,13 +1,18 @@
 
+#To find a substring's positions within a string 
 def Motif(S,T):
     pos = ''
+    #First start with first pos and as you go through S
     for i in range(len(S)):
+        #check if it matches with the first letter of T
         if S[i] == T[0]:
+            #see if the lenght of T is in S and matches
             if S[i:i+len(T)] == T:
+                #add the position(add 1 for index) to postion variable and add a space 
                 pos += str(i+1)+' '
     print (pos)
 
-
+#Define and call
 S = 'GATATATGCATATACTT'
 T= 'ATAT'
 
